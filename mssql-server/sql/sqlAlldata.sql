4789bfab7f0639ee12a1773c7a964dfca235f020
Thiago Oliveira <thiago-oliveira@live.com>
2024-08-09 22:08:53 -0300

    Update SQL Files

--- mssql-server/sql/sqlAlldata.sql
+++ mssql-server/sql/sqlAlldata.sql
@@ -76,30 +76,31 @@ INSERT INTO supermarket.dbo.productType (name,description,sector,id_tx) VALUES
 	 (N'Alcoholic beverages',N'All types of drinks that contain alcohol in their composition',N'Beverage',3),
 	 (N'Hygiene and Beauty',N'Personal hygiene products',N'Hygienics',4),
 	 (N'Cold and dairy products',N'All types of cold and dairy products',N'Colds',5),
-	 (N'Cleaning Supplies',N'All types of cleaning products',N'Cleaning',6);
+	 (N'Cleaning Supplies',N'All types of cleaning products',N'Cleaning',5);
 
 INSERT INTO supermarket.dbo.product (name,description,quantity,id_pt,value) VALUES
-	 (N'Heineken',N'350ml bottle',70,3,7.99),
-	 (N'Cheetos',N'250g pack',50,2,4.30),
-	 (N'hair cream',N'500ml pack',23,4,10.50),
-	 (N'Shampoo',N'250ml pack',5,4,18.99),
-	 (N'Conditioner',N'300ml pack',20,4,21.00),
-	 (N'Razor blade',N'3x Pack',17,4,21.90),
-	 (N'Soap ',N'250g Dove',92,4,10.20),
-	 (N'Deodorant',N'Rexona',25,4,4.30),
-	 (N'Toilet paper',N'Papaer More 5m',39,4,10.50),
-	 (N'Body cream',N'250ml Pantene',12,4,18.99);
+	 (N'Heineken',N'350ml bottle',70,1,7.99),
+	 (N'Cheetos',N'250g pack',50,2,1.30),
+	 (N'hair cream',N'500ml pack',23,3,10.50),
+	 (N'Shampoo',N'250ml pack',5,3,18.99),
+	 (N'Conditioner',N'300ml pack',20,3,21.00),
+	 (N'Razor blade',N'3x Pack',17,3,21.90),
+	 (N'Soap ',N'250g Dove',92,3,10.20),
+	 (N'Deodorant',N'Rexona',25,3,4.30),
+	 (N'Toilet paper',N'Papaer More 5m',39,3,10.50),
+	 (N'Body cream',N'250ml Pantene',12,3,18.99);
+
 INSERT INTO supermarket.dbo.product (name,description,quantity,id_pt,value) VALUES
-	 (N'Toothpaste',N'90g Colgate',37,4,21.00),
-	 (N'Cheese ',N'500g cheese calie',42,5,21.90),
-	 (N'Yogurts ',N'250g Vigor',90,5,7.20),
-	 (N'Milk ',N'1L Parmalate',120,5,18.21),
-	 (N'Vegetable milks ',N'1L Milkaas',18,5,10.99),
-	 (N'Bleach',N'Omo 800ml',23,7,21.82),
-	 (N'Detergents',N'Limpol 500ml',42,7,1.44),
-	 (N'Dishwasher detergent',N'Minuando 500ml',232,7,2.99),
-	 (N'Softener',N'Fofo 1L',50,7,13.00),
-	 (N'Liquid soap',N'Lux 250ml',44,7,7.40);
+	 (N'Toothpaste',N'90g Colgate',37,3,21.00),
+	 (N'Cheese ',N'500g cheese calie',42,4,21.90),
+	 (N'Yogurts ',N'250g Vigor',90,5,4.20),
+	 (N'Milk ',N'1L Parmalate',120,4,18.21),
+	 (N'Vegetable milks ',N'1L Milkaas',18,4,10.99),
+	 (N'Bleach',N'Omo 800ml',23,5,21.82),
+	 (N'Detergents',N'Limpol 500ml',42,5,1.44),
+	 (N'Dishwasher detergent',N'Minuando 500ml',232,5,2.99),
+	 (N'Softener',N'Fofo 1L',50,5,13.00),
+	 (N'Liquid soap',N'Lux 250ml',44,3,7.40);
 INSERT INTO supermarket.dbo.product (name,description,quantity,id_pt,value) VALUES
-	 (N'Flannels',N'Pack with 10',19,7,16.30),
+	 (N'Flannels',N'Pack with 10',19,5,16.30),
 	 (N'Testesds',N'Tedsdasste',222,5,5.00);
